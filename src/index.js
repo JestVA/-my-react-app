ba46c2d3acfcd6af6d65ffa80e16b0148b695e11
Dorin Dumitrascuta <d.dumitrascuta@gmail.com>
2018-10-24 12:06:03 +0100

    react changes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
+import './index.css'
 import React from "react";
 import ReactDOM from "react-dom";
 
+
 function Hi() {
-  return <div>Hello World!</div>;
+  return <div>{5+10}Hello <strong>Dorin!</strong>
+  <h1>This is a React app!</h1>
+  <ul>
+    <li>Main benefit is</li> 
+    <li>plenty</li>
+  </ul>
+  </div>;
 }
 
 ReactDOM.render(<Hi />, document.querySelector("#root"));
